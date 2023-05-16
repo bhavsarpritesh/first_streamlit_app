bfbf7107720e69ba0df93ac3281192cf5e221c06
Pritesh Bhavsar <109649207+bhavsarpritesh@users.noreply.github.com>
2023-05-16 12:02:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 
 #new section to display
 streamlit.header("Fruityvice Fruit Advice!")
-ruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
 
